91e10dcbf40277cd01c7f6076ca8283d0b0ad4ee
Charles Huang <cckhuang@users.noreply.github.com>
2025-11-11 18:42:45 -0500

    Added basic features to ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,26 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike: () => {this.setState((prevState) => ({likes: prevState.likes + 1}))},
+      handleDislke: () => {this.setState((prevState) => ({dislikes: prevState.dislikes + 1}))}
+    }
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+       <div class='content-rating'>
+        <p>
+          In React class components, this.setState is used to update the component's state
+          and trigger a re-render. There are two primary syntaxes for this.setState.
+        </p>
+        <div class='rating-buttons'>
+            <button class='like-button' onClick={this.state.handleLike}>Like({this.state.likes})</button>
+            <button class='dislike-button' onClick={this.state.handleDislke}>Dislike({this.state.dislikes})</button>
+        </div>
+       </div>
      </>
     );
   }
