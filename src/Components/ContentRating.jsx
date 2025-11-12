6b1054ac363ef457af653ea77307bedae2470f40
Charles Huang <cckhuang@users.noreply.github.com>
2025-11-11 20:07:37 -0500

    Fixed indentations in ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -25,19 +25,19 @@ class ContentRating extends Component {
   }
   render() {
     return (
-     <>
-       <div class='content-rating'>
-        <p>
-          In React class components, this.setState is used to update the component's state
-          and trigger a re-render. There are two primary syntaxes for this.setState.
-        </p>
-        <div class='rating-buttons'>
+      <>
+        <div class='content-rating'>
+          <p>
+            In React class components, this.setState is used to update the component's state
+            and trigger a re-render. There are two primary syntaxes for this.setState.
+          </p>
+          <div class='rating-buttons'>
             <button class='like-button' onClick={this.state.handleLike}>Like({this.state.likes})</button>
             <button class='dislike-button' onClick={this.state.handleDislke}>Dislike({this.state.dislikes})</button>
             <label for='totalRating'>Total Ratings: </label>
             <input type='number' id='totalRatings' name='totalRatings' value={this.state.totalRatings}/>
+          </div>
         </div>
-       </div>
      </>
     );
   }
